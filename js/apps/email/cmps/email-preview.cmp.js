eca4221f0b8b5441f154ee556458f4104bb66e01
DeeM <49118076+oxyhertz@users.noreply.github.com>
2022-03-06 09:22:52 +0200

    sd

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -1,4 +1,4 @@
-import { eventBus } from '../../../services/eventBus-service.js'
+import { eventBus } from '../../../services/eventBus-service.js';
 
 export default {
   props: ['email'],
@@ -12,11 +12,11 @@ export default {
                 <div class="preview-email-body"> -  {{emailTxt}} <span class="preview-date">{{time}} </span></div>
           </div>
                 <div class="actions">
-                   <i @click.stop="remove(email.id)" class="fa-solid fa-trash-can" class="preview-btn"></i>
-                   <i  v-if="email.isRead" class="fa-solid fa-envelope-open" class="preview-btn" @click.stop="save(email)"></i>
-                   <i  v-else class="fa-solid fa-envelope" class="preview-btn" @click.stop="save(email)"></i>
+                   <i title="remove" @click.stop="remove(email.id)" class="fa-solid fa-trash-can" class="preview-btn"></i>
+                   <i title="Readen" v-if="email.isRead" class="fa-solid fa-envelope-open" class="preview-btn" @click.stop="save(email)"></i>
+                   <i title="Unreaden" v-else class="fa-solid fa-envelope" class="preview-btn" @click.stop="save(email)"></i>
                    <i class="fa-solid fa-clock" class="preview-btn"></i>
-                   <i @click.stop="sendEmailAsNote(email)" class="fa-solid fa-note-sticky" class="preview-btn"></i>
+                   <i title="Send as note" @click.stop="sendEmailAsNote(email)" class="fa-solid fa-note-sticky" class="preview-btn"></i>
                 </div>
          </div>
      </section>
@@ -24,63 +24,73 @@ export default {
 
   methods: {
     save(email) {
-      email.isRead = !email.isRead
-      eventBus.emit('save', email)
-      
+      email.isRead = !email.isRead;
+      eventBus.emit('save', email);
     },
     remove(id) {
-      eventBus.emit('removeEmail', id)
+      eventBus.emit('removeEmail', id);
       eventBus.emit('show-msg', {
         txt: 'Email Removed',
         type: 'success',
       });
     },
     star(id) {
-      eventBus.emit('starEmail', id)
+      eventBus.emit('starEmail', id);
       eventBus.emit('show-msg', {
         txt: 'Email Starred',
         type: 'success',
       });
     },
     sendEmailAsNote(email) {
-      let subject = email.subject
-      let body = email.body
-      this.$router.push(`/notes?subject=${subject}&body=${body}`)
+      let subject = email.subject;
+      let body = email.body;
+      this.$router.push(`/notes?subject=${subject}&body=${body}`);
     },
   },
   computed: {
     time() {
-      var currDate = new Date()
-      var day = 60 * 60 * 24 * 1000
-      var year = day * 365
-      var sentAt = new Date(this.email.sentAt)
+      var currDate = new Date();
+      var day = 60 * 60 * 24 * 1000;
+      var year = day * 365;
+      var sentAt = new Date(this.email.sentAt);
       if (currDate - sentAt < day)
-      return sentAt.toLocaleTimeString('en-US', { hour12: true, hour: '2-digit', minute: '2-digit' })
-      if (currDate - sentAt > day  && currDate - sentAt < year )
-      return  sentAt.toLocaleString('en-US', { month: 'short', day: 'numeric' })
+        return sentAt.toLocaleTimeString('en-US', {
+          hour12: true,
+          hour: '2-digit',
+          minute: '2-digit',
+        });
+      if (currDate - sentAt > day && currDate - sentAt < year)
+        return sentAt.toLocaleString('en-US', {
+          month: 'short',
+          day: 'numeric',
+        });
       if (currDate - sentAt > year)
-      return sentAt.toLocaleString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })
+        return sentAt.toLocaleString('en-US', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+        });
     },
 
     isStar() {
-      if (this.email.isDeleted) return 'dont-show'
-      if (this.email.isStarred) return 'isStar'
+      if (this.email.isDeleted) return 'dont-show';
+      if (this.email.isStarred) return 'isStar';
     },
     isRead() {
       if (!this.email.isRead)
-        return { read: [this.email.to, this.email.subject] }
+        return { read: [this.email.to, this.email.subject] };
     },
     isReadBgc() {
       if (!this.email.isRead)
-        return { bgc: [this.email.to, this.email.subject] }
+        return { bgc: [this.email.to, this.email.subject] };
     },
     Sender() {
-      return this.email.to.substring(0, [this.email.to.indexOf('@')])
+      return this.email.to.substring(0, [this.email.to.indexOf('@')]);
     },
     emailTxt() {
       if (this.email.body.length > 30)
-        return this.email.body.substring(0, 30) + '...'
-      else return this.email.body
+        return this.email.body.substring(0, 30) + '...';
+      else return this.email.body;
     },
   },
-}
+};
